258c1391bc72acfd11b0d187d95dab7e65f7056d
Mauro Leonelli <mleonelli@gmail.com>
2021-05-27 08:39:50 +0000

    v.0.0.1

--- budget.py
+++ budget.py
@@ -1,59 +1,106 @@
-import builtins as __builtin__
-
-
-
-class Entry: 
-  amount = 0
-  description = ""
-
-  def __init__(self, amount,description):
-    amount = amount
-    description = description
-
-
-
 class Category:
-  ledger = []
-  _category = None
-  _balance = 0
 
   def __init__(self,category):
-    _category = category
+    self.category = category
+    self.ledger = []
+    self.balance = 0
   
   def __str__(self):
-    return "My overridden print() function!"
+    output = ""
+    l = int((30 - len(self.category)) / 2)
+    
+    output += self.add_asterisks(l)
+    output += self.category
+    output += self.add_asterisks(l) + "\n"
 
-  def deposit(self, amount, description):
-    entry = Entry(amount, description)
-    ledger.append(entry)
-    _balance += amount
+    for entry in self.ledger:
+      output += entry["description"][:23] + (" " * (23 - len(entry["description"][:23])))
+      output += " "
+      a = format(entry["amount"], '.2f')
+      output += " " * (7 - len(str(a))) + a + "\n"
+    
+    output += "Total: " + str(self.balance)
+    return output
 
-  def withdraw(amount, description):
-    if this.check_funds(amount):
-      return False
-    else:
-      entry = Entry(-amount, description)
-      ledger.append(entry)
-      _balance -= amount
+  def add_asterisks(self, l):
+    return "*" * l
+
+  def deposit(self, amount, description = None):
+    if description is None:
+      description = ""
+    self.ledger.append({"amount":amount,"description":description})
+    self.balance += amount
+
+  def withdraw(self, amount, description =None):
+    if self.check_funds(amount):
+      if description is None:
+        description = ""
+      self.ledger.append({"amount":-amount,"description":description})
+      self.balance -= amount
       return True
+    return False
 
-  def get_balance():
-    return _balance
+  def get_balance(self):
+    return self.balance
 
-  def transfer(amount, budget):
-    if check_funds(amount):
-      return False
-    else:
-      this.withdraw(amount, "Transfer to " + budget._category)
-      budget.deposit(amount, "Transfer from " + _category)
+  def transfer(self, amount, budget):
+    if self.check_funds(amount):
+      self.withdraw(amount, "Transfer to " + budget.category)
+      budget.deposit(amount, "Transfer from " + self.category)
       return True
+    return False
+      
 
-  def check_funds(amount):
-    if amount > _balance:
+  def check_funds(self, amount):
+    if amount > self.balance:
       return False
     else:
       return True
 
 
 def create_spend_chart(categories):
-  return ""
+  output = "Percentage spent by category\n"
+  spent = []
+  lens = []
+  total = 0
+
+  for c in categories:
+    spent.append(c.balance)
+    lens.append(len(c.category))
+    total += c.balance
+
+  maxL = max(lens)
+
+  for i in range(len(spent)):
+    spent[i] = approximate_10(int((spent[i] / total) * 100))
+
+  for i in range(100, 0, -10):
+    output += " " * (3 - len(str(i))) + str(i) + "| "
+    for j in spent:
+      if j >= i:
+        output += "o "
+      else:
+        output += " " * 2
+    output += "\n"
+
+  output += (" " * 4) + "----------" + "\n"
+
+  for i in range(0, maxL):
+    output += (" " * 5)
+    for c in categories:
+      try:
+        output += c.category[i] + " "
+      except:
+        output += " " * 2
+    output += "\n"
+
+  #print(spent)
+
+  return output
+
+def approximate_10(n):
+  #print(n)
+  r = n % 10
+  if r < 5:
+    return n - r
+  return n - r + 10
--- test_module.py
+++ test_module.py
@@ -79,7 +79,10 @@ class UnitTests(unittest.TestCase):
         self.food.withdraw(45.67, "milk, cereal, eggs, bacon, bread")
         self.food.transfer(20, self.entertainment)
         actual = str(self.food)
+        
         expected = f"*************Food*************\ndeposit                 900.00\nmilk, cereal, eggs, bac -45.67\nTransfer to Entertainme -20.00\nTotal: 834.33"
+        print(actual)
+        print(expected)
         self.assertEqual(actual, expected, 'Expected different string representation of object.')
 
     def test_create_spend_chart(self):
